80ea243793533553fb342b5d2774bf56698529c3
Dan Pasanen <dan.pasanen@gmail.com>
2015-10-07 10:57:51 -0500

    power: don't build aosp hal if using caf
    Change-Id: Ic16d96d8185d03354091386f4224e23e2c479894

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
+
 ifneq ($(filter msm8960 msm8974,$(TARGET_BOARD_PLATFORM)),)
 LOCAL_PATH := $(call my-dir)
 
@@ -14,4 +16,6 @@ LOCAL_SRC_FILES := \
 LOCAL_MODULE:= power.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
+endif
+
 endif
